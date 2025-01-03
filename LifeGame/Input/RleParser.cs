92baa80ad625bc92d775374eb8b5cda3499a3690
acple <silentsphere110@gmail.com>
2025-01-03 16:49:24 +0900

    update nuget packages

--- LifeGame/Input/RleParser.cs
+++ LifeGame/Input/RleParser.cs
@@ -9,9 +9,9 @@ public class RleParser : IParser
 {
     private record State(int X, int Y, ImmutableHashSet<Cell> Cells);
 
-    private static readonly Parser<char, Board> parser = CreateParser();
+    private static readonly IParser<char, Board> parser = CreateParser();
 
-    private static Parser<char, Board> CreateParser()
+    private static IParser<char, Board> CreateParser()
     {
         var whitespace = OneOf(" \t").Or(EndOfLine()).Ignore();
         var comment = Char('#').Right(Match(EndOfLine().Ignore() | EndOfInput()));
@@ -26,25 +26,20 @@ public class RleParser : IParser
         var aliveCell = Char('o').Right(spacing);
         var end = Char('!').Right(spacing);
 
-        Parser<char, State> Newline(int count, State state)
+        IParser<char, State> Newline(int count, State state)
             => newline.Map(_ => state with { X = 0, Y = state.Y + count });
 
-        Parser<char, State> DeadCell(int count, State state)
+        IParser<char, State> DeadCell(int count, State state)
             => deadCell.Map(_ => state with { X = state.X + count });
 
-        Parser<char, State> AliveCell(int count, State state)
+        IParser<char, State> AliveCell(int count, State state)
             => aliveCell.Map(_ => state with { X = state.X + count, Cells = state.Cells.Union(Enumerable.Range(state.X, count).Select(x => new Cell(x, state.Y))) });
 
         var rle = Pure(new State(0, 0, []))
             .Chain(state => count.Bind(count => Choice(Newline(count, state), DeadCell(count, state), AliveCell(count, state))))
             .Left(end);
 
-        var parser =
-            from _0 in spacing
-            from _1 in header
-            from data in rle
-            from _2 in EndOfInput()
-            select new Board(data.Cells);
+        var parser = Sequence(spacing, header, rle, EndOfInput(), (_, _, data, _) => new Board(data.Cells));
 
         return parser;
     }
